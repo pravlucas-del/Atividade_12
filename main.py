e7cd92e937197f3a0e11bbe744ac2e77b2d9ca85
Lucas Praciano Soares <pravlucas@gmail.com>
2026-06-03 12:24:20 -0300

    Terminar de construir o mapa

--- main.py
+++ main.py
@@ -9,30 +9,33 @@ clock = time.Clock()
 
 #mapa = image.load("Atividade_12/Cenario/masmorra.png")
 #mapa = transform.scale(mapa, (1280, 720))  # Redimensiona o mapa para preencher a tela
-mapa_2 = image.load("Atividade_12/cenario/masmorra.png")
+mapa_2 = image.load("cenario/masmorra.png")
 pos_x = 0
 pos_y = 0
+
+mapa_3 = image.load("sprites_separados/sprite_4.png")
+mapa_4 = image.load("spritesheet.png")
 
 # Variáveis para animação da vela tipo A
 curr_frame_vela = 0
 anim_timer_vela = 0
 vela = []
 for i in range(4):
-    vela.append(image.load(f"Atividade_12/cenario/candleA_0{i+1}.png"))
+    vela.append(image.load(f"cenario/candleA_0{i+1}.png"))
 
 # Variáveis para animação da vela tipo B
 curr_frame_vela_b = 0
 anim_timer_vela_b = 0
 vela_b = []
 for i in range(4):
-    vela_b.append(image.load(f"Atividade_12/cenario/candleB_0{i+1}.png"))
+    vela_b.append(image.load(f"cenario/candleB_0{i+1}.png"))
 
 # Variáveis dos espinhos
 curr_frame_spike = 0
 anim_timer_spike = 0
 spike = []
-for i in range(5):
-    spike.append(image.load(f"Atividade_12/cenario/spike_0{i+1}.png"))
+#for i in range(5):
+    #spike.append(image.load(f"cenario/spike_0{i+1}.png"))
 
 mapa_joguinho = [
     "XXXXXXXXXXXXXX",
@@ -79,11 +82,11 @@ while True:
     anim_timer_spike += dt
     anim_timer_sec_spike = anim_timer_spike / 1000
 
-    if anim_timer_sec_spike >= 0.25:
-        curr_frame_spike += 1
-        if curr_frame_spike >= len(spike):
-            curr_frame_spike = 0
-        anim_timer_spike = 0
+    #if anim_timer_sec_spike >= 0.25:
+    #    curr_frame_spike += 1
+    #    if curr_frame_spike >= len(spike):
+    #        curr_frame_spike = 0
+    #    anim_timer_spike = 0
     
     
     old_pos_x = pos_x
@@ -117,14 +120,14 @@ while True:
 
 
 
-    screen.fill((0, 0, 0))  # Limpa a tela com cinza
-    
+    screen.fill((255, 255, 255))  # Limpa a tela com cinza
+    #screen.fill((0,0,0))
     # Renderização da vela animada
     screen.blit(vela[curr_frame_vela], (200,200))
     screen.blit(vela_b[curr_frame_vela_b], (400,200))
 
     # Renderização dos espinhos animados
-    screen.blit(spike[curr_frame_spike], (600,200))
+    #screen.blit(spike[curr_frame_spike], (600,200))
 
     # Renderiza o mapa (cenário)
     screen.blit(mapa_2, (-55, -40),(0,0,290,190) )  # Desenha o cenário na posição 
@@ -135,7 +138,76 @@ while True:
     screen.blit(mapa_2, (950, -40),(0,0,290,190) )  # Desenha o cenário na posição
     screen.blit(mapa_2, (1150, -40),(0,0,290,190) )  # Desenha o cenário na posição
     
+    # Lado Direito
+    screen.blit(mapa_3, (600,600),(0,200,20,32))
+    screen.blit(mapa_3, (610,600),(0,200,20,32))
+    screen.blit(mapa_3, (600,610),(0,200,20,32))
+    screen.blit(mapa_3, (610,610),(0,200,20,32))
+    screen.blit(mapa_3, (610,620),(0,200,20,32))
+    screen.blit(mapa_3, (610,630),(0,200,20,32))
+    screen.blit(mapa_3, (610,640),(0,200,20,32))
+    screen.blit(mapa_3, (610,650),(0,200,20,32))
+    screen.blit(mapa_3, (610,660),(0,200,20,32))
+    screen.blit(mapa_3, (610,670),(0,200,20,32))
+    screen.blit(mapa_3, (610,680),(0,200,20,32))
+    screen.blit(mapa_3, (610,690),(0,200,20,32))
+    screen.blit(mapa_3, ( 610,590), (0,200,20,32))
+    screen.blit(mapa_3, ( 620,590), (0,200,20,32))
+    screen.blit(mapa_3, ( 630,590), (0,200,20,32))
+    screen.blit(mapa_3, ( 640,590), (0,200,20,32))
+    
+    screen.blit(mapa_3, ( 590,600), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,610), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,620), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,630), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,640), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,650), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,660), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,670), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,680), (0,200,20,32))
+    screen.blit(mapa_3, ( 590,690), (0,200,20,32))
+    screen.blit(mapa_3, (630,600),(0,200,20,32))
+    screen.blit(mapa_3, (630,610),(0,200,20,32))
+    screen.blit(mapa_3, (630,650),(0,200,20,32))
+    screen.blit(mapa_3, (630,660),(0,200,20,32))
+    
 
+    # Lado Esquerdo
+    screen.blit(mapa_3, (600,620),(0,200,20,32))
+    screen.blit(mapa_3, (600,630),(0,200,20,32))
+    screen.blit(mapa_3, (600,640),(0,200,20,32))
+    screen.blit(mapa_3, (600,650),(0,200,20,32))
+    screen.blit(mapa_3, (600,660),(0,200,20,32))
+    screen.blit(mapa_3, (600,670),(0,200,20,32))
+    screen.blit(mapa_3, (600,680),(0,200,20,32))
+    screen.blit(mapa_3, (600,690),(0,200,20,32))
+
+    screen.blit(mapa_3, (620,600),(0,200,20,32))
+    screen.blit(mapa_3, (620,610),(0,200,20,32))
+    screen.blit(mapa_3, (620,620),(0,200,20,32))
+    screen.blit(mapa_3, (620,630),(0,200,20,32))
+    screen.blit(mapa_3, (620,640),(0,200,20,32))
+    screen.blit(mapa_3, (620,650),(0,200,20,32))
+    screen.blit(mapa_3, (620,660),(0,200,20,32))
+    screen.blit(mapa_3, (620,670),(0,200,20,32))
+    screen.blit(mapa_3, (620,680),(0,200,20,32))
+    screen.blit(mapa_3, (620,690),(0,200,20,32))
+    screen.blit(mapa_3, (600,590),(0,200,20,32))
+    screen.blit(mapa_3, (590,590),(0,200,20,32))
+    
+    # Paredes Lado Direito
+    screen.blit(mapa_4,(610,600),(0,0,47,150))
+    screen.blit(mapa_4,(610,550),(0,0,47,150))
+
+    # Paredes Lado Esquerdo
+    screen.blit(mapa_4,(390,600),(0,0,100,200))
+    
+
+
+
+
+
+    screen.blit(mapa_3,(110,200),(0,200,20,32))
 
 
 
